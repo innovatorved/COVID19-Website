43c77f625d905faae1c3edce0a8ad25360682a02
Ved Gupta <nextin.520@gmail.com>
2020-12-19 16:26:29 +0530

    Flask Server ready & value were start recieving

--- files/main.py
+++ files/main.py
@@ -1,26 +1,38 @@
 #flash framework - which is micro framework
 
 from flask import Flask
-from flask import render_template ,redirect ,url_for
+from flask import request
+import requests
+from flask import render_template ,redirect ,url_for ,jsonify
 
+#list name
+data = []
 
-
+        
 app = Flask(__name__)
 
-@app.route('/h', methods = ['GET','POST'])
+@app.route('/', methods = ['GET','POST'])
 def index():
 	if request.method == 'POST':
         #print('done')
 		if 'name' in request.form and 'email'  in request.form:
 			name = request.form['name']
 			email = request.form['email']
-			print(name)
-			print(email)
+			global data
+			data.append({"name":name , "email":email})
+			#print(data)
+			return "<script>window.alert('Sucessfully Registered');</script>"
+		#else:
+                        #print("")
 	return render_template("index.html")
 
-@app.route('/')
-def hello():
-	return "hello"
-	
+@app.route('/data')
+def dataAll():
+	return jsonify(data)
+
+@app.route('/api/<string:n>/<string:m>/')
+def add(n,m):
+      pass
+
 if __name__ == '__main__':
         app.run(debug=True, use_reloader=False)
