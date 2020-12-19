fbb1bea2b3e90210839d0e3d8909497f27b8edc0
Ved Gupta <nextin.520@gmail.com>
2020-12-19 22:40:53 +0530

    file copy to main

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
- 
+
 CLIENT_SECRET_FILE = 'credentials.json'
 API_NAME = 'gmail'
 API_VERSION = 'v1'
@@ -18,7 +18,7 @@ mimeMessage.attach(MIMEText(emailMsg, 'plain'))
 raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
  
 message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-print(message)
+#print(message)
 
 def email(Msg , email , subject):
     emailMsg = Msg
@@ -26,5 +26,9 @@ def email(Msg , email , subject):
     mimeMessage['to'] = email
     mimeMessage['subject'] = subject
     mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    
+    html_body_tag = "<body>-------</body>"
+    msg.attach(TexT(html_body_tag , 'html'))
+    
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
     message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
