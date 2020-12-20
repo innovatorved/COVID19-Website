8bce60faa4b993672e21643d2ca83679cbc7487f
Ved Gupta <nextin.520@gmail.com>
2020-12-20 17:33:21 +0530

    basic changs

--- main.py
+++ main.py
@@ -4,26 +4,29 @@ from flask import Flask
 from flask import request
 import requests
 from flask import render_template ,redirect ,url_for ,jsonify
+import os
 
-#list name
+# list name
 data = []
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+port = int(os.getenv('PORT', 8000))
 
 @app.route('/', methods = ['GET','POST'])
 def index():
 	if request.method == 'POST':
-        #print('done')
+        # print('done')
 		if 'name' in request.form and 'email'  in request.form:
 			name = request.form['name']
 			email = request.form['email']
 			global data
 			data.append({"name":name , "email":email})
-			#print(data)
+			# print(data)
 			return "<script>window.alert('Sucessfully Registered');</script>"
-		#else:
-                        #print("")
-	return render_template("index.html")
+		# else:
+                        # print("")
+	return app.send_static_file("index.html")
 
 @app.route('/data')
 def dataAll():
@@ -33,9 +36,16 @@ def dataAll():
 def page_not_found(e):
     return render_template('500.html'), 404
 
+# inbuilt function which takes error as parameter
+@app.errorhandler(404)  
+def not_found(e): 
+    # defining function
+    return render_template("404.html")
+
 @app.route('/api/<string:n>/<string:m>/')
 def add(n,m):
       pass
 
 if __name__ == '__main__':
-        app.run(debug=True, use_reloader=False)
+    # host='0.0.0.0', 
+        app.run(port=port ,debug=True, use_reloader=False)
